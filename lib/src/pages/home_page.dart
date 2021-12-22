fd804d799eb9bef4afae257a4153b73b1829e8a8
EvelynGalarza <ejgalarza1@espe.edu.ec>
2021-12-22 17:35:02 -0500

    TC Shared Preferences

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:prylibro/src/providers/providers.dart';
 import 'package:prylibro/src/widgets/lectura_widget.dart';
+import 'package:flutter_speed_dial/flutter_speed_dial.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -8,10 +12,28 @@ class HomePage extends StatefulWidget {
   _HomePageState createState() => _HomePageState();
 }
 
+List fonts = [
+  GoogleFonts.aBeeZee,
+  GoogleFonts.pacifico,
+  GoogleFonts.lobster,
+  GoogleFonts.combo,
+];
+
+var fontName = <String>[
+  'aBeeZee',
+  'pacifico',
+  'lobster',
+  'combo',
+];
+int myindex = 0;
+String _selectedFont = "aBeeZee";
+
 class _HomePageState extends State<HomePage> {
+  final temaController = Get.put(TemaProvider());
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        body: const LecturaWidget(),
         appBar: AppBar(
             title: const Text("Biblioteca"),
             leading: GestureDetector(
@@ -34,9 +56,29 @@ class _HomePageState extends State<HomePage> {
                   padding: const EdgeInsets.only(right: 20.0),
                   child: GestureDetector(
                     onTap: () {},
-                    child: const Icon(Icons.more_vert),
+                    child: const Icon(Icons.light),
                   )),
             ]),
-        body: const LecturaWidget());
+        floatingActionButton: SpeedDial(
+          animatedIcon: AnimatedIcons.view_list,
+          children: [
+            SpeedDialChild(
+              child: const Icon(Icons.wb_sunny),
+              label: "claro",
+              onTap: () => temaController.temaClaro(),
+            ),
+            SpeedDialChild(
+              child: const Icon(Icons.mode_night),
+              label: "oscuro",
+              onTap: () => temaController.temaOscuro(),
+            ),
+            SpeedDialChild(
+              child: const Icon(Icons.color_lens),
+              label: "Personalizar",
+              onTap: () => temaController.temaPersonalizado(),
+            ),
+          ],
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.endFloat);
   }
 }
---
+++ lib/src/providers/providers.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+Map<int, Color> color = {
+  50: const Color(0xFF3E2158), //10%
+  100: const Color(0xFF030118), //20%
+  200: const Color(0xFFFAF7F6), //30%
+  300: const Color(0xA6A69FF3), //40%
+  400: const Color(0xA6564BCA), //50%
+  500: const Color(0xA6322994), //60%
+  600: const Color(0xA6201A64), //70%
+  700: const Color(0xA619144E), //80%
+  800: const Color(0xA6140F46), //90%
+  900: const Color(0xA608061D), //100%
+};
+MaterialColor colorCustom = MaterialColor(0xFF3B4499, color);
+
+class TemaProvider extends GetxController {
+  late SharedPreferences preferences;
+
+  String prefkey = "isDarkModeKey";
+
+  void temaClaro() {
+    Get.changeTheme(ThemeData.light());
+    preferences.setBool(prefkey, false);
+  }
+
+  void temaOscuro() {
+    Get.changeTheme(ThemeData.dark());
+    preferences.setBool(prefkey, true);
+  }
+
+  void temaPersonalizado() {
+    Get.changeTheme(ThemeData(
+        buttonColor: Colors.amber,
+        primaryColor: Colors.black,
+        appBarTheme: AppBarTheme(color: Colors.lightGreen)));
+  }
+
+  @override
+  void onInit() {
+    cargarPreferencias().then((value) => cargarTema());
+
+    super.onInit();
+  }
+
+  void cargarTema() {
+    bool? isDarkMode = preferences.getBool(prefkey);
+
+    if (isDarkMode == null) {
+      preferences.setBool(prefkey, false);
+      isDarkMode = false;
+    }
+
+    (isDarkMode) ? temaOscuro() : temaClaro();
+  }
+
+  Future<void> cargarPreferencias() async {
+    preferences = await Get.putAsync<SharedPreferences>(
+        () async => await SharedPreferences.getInstance());
+  }
+}
